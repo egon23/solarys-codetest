5d68fa91cd5a8c10b767625fbe3410c207c2aa5d
Manya Egon <egon.manya@gmail.com>
2024-07-30 15:23:13 +0300

    Initial commit

---
+++ solarys-codetest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  solarys-codetest
+//
+//  Created by Egon Manya on 30.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ solarys-codetest/solarys_codetestApp.swift
@@ -0,0 +1,17 @@
+//
+//  solarys_codetestApp.swift
+//  solarys-codetest
+//
+//  Created by Egon Manya on 30.07.24.
+//
+
+import SwiftUI
+
+@main
+struct solarys_codetestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
